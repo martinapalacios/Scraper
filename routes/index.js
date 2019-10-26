f698e1d4b6d9e0dd8f03a3af2c99a92862ae79d6
Martin P <mpala1433@gmail.com>
2019-10-26 14:08:18 -0500

    fix errors

--- routes/index.js
+++ routes/index.js
@@ -12,33 +12,39 @@ router.get("/", function(req, res) {
 
 // Scrape from Source
 router.get("/scrape", function(req, res) {
-  axios.get("https://bids.lja.com/", function(error, response, html) {
+  axios.get("http://bids.lja.com/").then(function(response) {
+    // IF an error happens then exit out while displaying error
+    console.log("Scrapping started");
+    // if (error) return res.send(error);
+    // console.log(response);
+
     var scraped = [];
 
-    var $ = cheerio.load(html);
+    var $ = cheerio.load(response.data);
 
-    $("div.block-content").each(function(i, element) {
-      var result = {};
+    $(".srl_results_entry div b").each(function(i, element) {
+      console.log(element);
+      // var result = {};
 
-      result.title = $(this)
-        .children("h2")
-        .text();
-      result.link = $(this)
-        .children("h2")
-        .children("a")
-        .attr("href");
-      result.excerpt = $(this)
-        .children("p.excerpt")
-        .text();
-      result.image = $(this)
-        .children("span")
-        .children("a.thumb")
-        .children("img")
-        .data("src");
+      // result.title = $(this)
+      //   .children("h2")
+      //   .text();
+      // result.link = $(this)
+      //   .children("h2")
+      //   .children("a")
+      //   .attr("href");
+      // result.excerpt = $(this)
+      //   .children("p.excerpt")
+      //   .text();
+      // result.image = $(this)
+      //   .children("span")
+      //   .children("a.thumb")
+      //   .children("img")
+      //   .data("src");
 
-      if (result.link) {
-        scraped.push(result);
-      }
+      // if (result.link) {
+      // scraped.push(result);
+      // }
     });
 
     res.locals.scrapeCount = scraped.length;
